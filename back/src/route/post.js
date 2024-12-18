fc5c798b1d50dc35a0481f99f260c8b88c0ea12b
Pozitivchik <maxymyurkovets@gmail.com>
2024-12-18 18:49:53 +0200

    complete twitter

---
+++ back/src/route/post.js
@@ -0,0 +1,134 @@
+// Підключаємо технологію express для back-end сервера
+const express = require('express')
+// Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
+const router = express.Router()
+
+const { Post } = require('../class/post')
+
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.post('/post-create', function (req, res) {
+  // res.render генерує нам HTML сторінку
+  try {
+    const { username, text, postId } = req.body
+
+    if (!username || !text) {
+      return res.status(400).json({
+        message:
+          'Потрібно передати всі дані для створення поста',
+      })
+    }
+
+    let post = null
+
+    console.log(postId, 'postId')
+
+    if (postId) {
+      post = Post.getById(Number(postId))
+      console.log(post, 'post')
+
+      if (!post) {
+        return res.status(400).json({
+          message: 'Пост з таким id не існує!',
+        })
+      }
+    }
+
+    const newPost = Post.create(username, text, post)
+
+    return res.status(200).json({
+      post: {
+        id: newPost.id,
+        text: newPost.text,
+        username: newPost.username,
+        date: newPost.date,
+      },
+    })
+  } catch (e) {
+    return res.status(400).json({
+      message: e.message,
+    })
+  }
+})
+
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.get('/post-list', function (req, res) {
+  // res.render генерує нам HTML сторінку
+  try {
+    const list = Post.getList()
+
+    if (list.length === 0) {
+      return res.status(200).json({
+        list: [],
+      })
+    }
+
+    return res.status(200).json({
+      list: list.map(({ id, username, text, date }) => ({
+        id,
+        username,
+        text,
+        date,
+      })),
+    })
+  } catch (e) {
+    return res.status(400).json({
+      message: e.message,
+    })
+  }
+})
+
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.get('/post-item', function (req, res) {
+  // res.render генерує нам HTML сторінку
+  try {
+    const { id } = req.query
+
+    if (!id) {
+      return res.status(400).json({
+        message: 'Потрібно передати ID поста',
+      })
+    }
+
+    const post = Post.getById(Number(id))
+
+    if (!post) {
+      return res.status(400).json({
+        message: 'Пост з таким ID не існує!',
+      })
+    }
+
+    return res.status(200).json({
+      post: {
+        id: post.id,
+        text: post.text,
+        username: post.username,
+        date: post.date,
+
+        reply: post.reply.map((reply) => ({
+          id: reply.id,
+          text: reply.text,
+          username: reply.username,
+          date: reply.date,
+        })),
+      },
+    })
+  } catch (e) {
+    return res.status(400).json({
+      message: e.message,
+    })
+  }
+})
+
+module.exports = router
---
+++ front/src/component/title/index.js
@@ -0,0 +1,4 @@
+import "./index.css";
+export default function Component({ children }) {
+  return <h1 className="title">{children}</h1>;
+}
